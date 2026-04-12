eb55e1b6357fdfd663040b01c798ac4cfcb0c4c7
Aiden Wong <ahcmwong@bu.edu>
2026-04-12 17:51:41 -0400

    re-did the test/validation split code, it didn't save previously

--- train.py
+++ train.py
@@ -6,12 +6,18 @@ from pathlib import Path
 
 import torch
 from torch.utils.data import DataLoader
+from torch.utils.data import random_split
 
 if __name__ == "__main__":
     # instanstiate the dataset, model, and loss.
     root = Path('/projectnb/cs585/projects/ASUFratLeader/data/Data/Closed_Set')
     wild_dataset = WILDDataset(root)
-    data_loader = DataLoader(wild_dataset, batch_size=128, shuffle=True, pin_memory=True)
+    train_size = int(0.8 * len(wild_dataset))
+    val_size = len(wild_dataset) - train_size
+    train_dataset, val_dataset = random_split(wild_dataset, [train_size, val_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False, pin_memory=True)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     drift = DRIFT().to(device)
